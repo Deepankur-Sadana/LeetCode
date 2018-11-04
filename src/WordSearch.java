46eeacb14ff217fa531b1e5d8ee37ba86cb14073
Deepankur <writetodeepankursadana@gmail.com>
2018-11-04 20:54:16 +0530

    #73 set-matrix-zeroes/

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -41,7 +41,7 @@ public class WordSearch {
 
 			{ -1, -1 }, { -1, 1 }, { 1, 1 }, { 1, -1 } };
 
-	//#661 Image smoother
+	// #661 Image smoother
 	public int[][] imageSmoother(int[][] M) {
 		int[][] res = new int[M.length][M[0].length];
 
@@ -49,7 +49,7 @@ public class WordSearch {
 			for (int c = 0; c < M[0].length; c++) {
 
 				int sum = M[r][c], count = 1;
-				
+
 				for (int[] dir : AllDirections) {
 					int x = r + dir[0], y = c + dir[1];
 					if (x < 0 || x >= M.length || y < 0 || y >= M[0].length)
@@ -57,11 +57,101 @@ public class WordSearch {
 					sum += M[x][y];
 					count++;
 				}
-				if(count != 0)
-				res[r][c] = sum / count;
+				if (count != 0)
+					res[r][c] = sum / count;
 			}
 		}
 		return res;
 	}
 
+	// #73 set-matrix-zeroes/
+	public void setZeroes(int[][] matrix) {
+		boolean col_0 = false// set first col to 0
+				, row_0 = false;// set first row to 0
+
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[i][0] == 0) {
+				col_0 = true;
+				break;
+			}
+		}
+
+		for (int i = 0; i < matrix[0].length; i++) {
+			if (matrix[0][i] == 0) {
+				row_0 = true;
+				break;
+			}
+		}
+
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[0].length; c++) {
+				if (matrix[r][c] == 0) {
+					matrix[r][0] = 0;
+					matrix[0][c] = 0;
+				}
+			}
+		}
+
+		for (int r = 1; r < matrix.length; r++) {
+			if (matrix[r][0] == 0)
+				nulliFyRow(r, matrix);
+		}
+		for (int c = 1; c < matrix[0].length; c++) {
+			if (matrix[0][c] == 0)
+				nulliFyCol(c, matrix);
+		}
+
+		if (col_0)
+			nulliFyCol(0, matrix);
+
+		if (row_0)
+			nulliFyRow(0, matrix);
+
+	}
+
+	void nulliFyRow(int row, int[][] matrix) {
+		for (int c = 0; c < matrix[row].length; c++) {
+			matrix[row][c] = 0;
+		}
+	}
+
+	void nulliFyCol(int col, int[][] matrix) {
+		for (int r = 0; r < matrix.length; r++) {
+			matrix[r][col] = 0;
+		}
+	}
+
+	int M = Integer.MIN_VALUE, sM = Integer.MIN_VALUE, tM = Integer.MIN_VALUE;
+	boolean MIN;
+
+	public int thirdMax(int[] nums) {
+		M = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			int num = nums[i];
+			if (num == Integer.MIN_VALUE)
+				MIN = true;
+			if (num > M) {
+
+			} else if (num < M && num > sM) {
+
+			} else if (num < sM && num > tM) {
+
+			}
+
+		}
+	}
+
+	public int findMaxConsecutiveOnes(int[] nums) {
+		int max = 0, len = 0;
+		for (int n : nums) {
+			if (n == 1)
+				++len;
+			else {
+				if (len > max)
+					max = len;
+				len = 0;
+			}
+		}
+		return Math.max(max, len);
+	}
 }
