42c05f115d9b24e9eb923f1f025cffea9cbd140f
Deepankur <writetodeepankursadana@gmail.com>
2018-11-07 17:25:25 +0530

    #461 Hamming Distance

--- src/Random.java
+++ src/Random.java
@@ -46,4 +46,20 @@ public class Random {
 
 	}
 
+	// #461 Hamming Distance
+	public int hammingDistance(int x, int y) {
+		int sum = 0;
+		int res = 0;
+		while (x != 0 || y != 0) {
+			res = x & 1;
+			if (res != (y & 1)) {
+				++sum;
+			}
+			x = x >> 1;
+			y = y >> 1;
+
+		}
+		return sum;
+	}
+
 }
